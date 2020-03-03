efd06319fbd8dfdc7f55d6cd33abec20b33d16c0
Riley Bowling <rbowling@bsd220.org>
2020-03-03 10:42:08 -0600

    Collected and passed data in segue

--- za hunter/LocationDetailsViewController.swift
+++ za hunter/LocationDetailsViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationDetailsViewController: UIViewController {
+    
+    var selectedMapItem = MKMapItem()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(selectedMapItem.name!)
     }
 }
--- za hunter/ViewController.swift
+++ za hunter/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     let shopAddress = ""
     let geocoder = CLGeocoder()
     var region = MKCoordinateRegion()
+    var mapItems = [MKMapItem]()
+    var selectedMapItem = MKMapItem()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,34 +45,50 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         request.region = region
         let search = MKLocalSearch(request: request)
         search.start { (response, error) in
-           if let response = response {
-              for mapItem in response.mapItems {
-                  let annotation = MKPointAnnotation()
-                  annotation.coordinate = mapItem.placemark.coordinate
-                  annotation.title = mapItem.name
-                  self.mapView.addAnnotation(annotation)
-              }
-           }
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
+                    self.mapItems.append(mapItem)
+                }
+            }
         }
     }
-
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
-           return nil
+            return nil
         }
         var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
         if pinView == nil {
-           pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
-           pinView?.canShowCallout = true
-           pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            pinView?.canShowCallout = true
+            pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
         } else {
-           pinView?.annotation = annotation
+            pinView?.annotation = annotation
         }
         return pinView
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         performSegue(withIdentifier: "ShowLocationDetailsSegue", sender: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        for mapItem in mapItems {
+            if mapItem.placemark.coordinate.latitude == view.annotation?.coordinate.latitude &&
+                 mapItem.placemark.coordinate.longitude == view.annotation?.coordinate.longitude {
+                    selectedMapItem = mapItem
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? LocationDetailsViewController {
+           destination.selectedMapItem = selectedMapItem
+        }
     }
 }
 
